d4864f1fdb7ef288731c07ca45a522d41747355b
Phil <philip.isenmann@gmail.com>
2025-05-07 17:29:52 +0200

    plain integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Lenis from "@studio-freight/lenis";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type AppType } from "next/dist/shared/lib/utils";
+import Script from "next/script";
 
 import "~/styles/globals.css";
 import { MeshProvider } from "@meshsdk/react";
@@ -37,6 +38,29 @@ const MyApp: AppType = ({ Component, pageProps }) => {
           <Component {...pageProps} />
         </Layout>
       </MeshProvider>
+      <Script id="plain-chatbot" strategy="lazyOnload">
+        {`
+          (function(d, script) {
+            script = d.createElement('script');
+            script.async = false; // Next/Script handles async, consider removing or testing
+            script.onload = function(){
+              window.Plain.init({
+                appId: 'liveChatApp_01JAFM75T0VXH1PM8Y7N08M47Q',
+                title: 'Welcome to the dRepWatch Support!',
+                theme: 'light',
+                requireAuthentication: true,
+                threadDetails: { labelTypeIds: ['lt_01JSVEXTW0J9CT6WPY6TK48MKC'], },
+                style: {
+                  chatButtonColor: '#000000',
+                  chatButtonIconColor: '#FF6641',
+                },
+              });
+            };
+            script.src = 'https://chat.cdn-plain.com/index.js';
+            d.getElementsByTagName('head')[0].appendChild(script);
+          }(document));
+        `}
+      </Script>
     </QueryClientProvider>
   );
 };
